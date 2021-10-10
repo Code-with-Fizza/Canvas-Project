dcd2d48177ec20ee1096985147a8638d322fda8d
Fizza <fizzanaeem20@gmail.com>
2021-10-10 02:48:05 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createDemoApp } from 'polotno/polotno-app';
+import { createStore } from 'polotno/model/store';
+import { PolotnoContainer, SidePanelWrap, WorkspaceWrap } from 'polotno';
+import { Toolbar } from 'polotno/toolbar/toolbar';
+import { ZoomButtons } from 'polotno/toolbar/zoom-buttons';
+import { SidePanel } from 'polotno/side-panel';
+import { Workspace } from 'polotno/canvas/workspace';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// import css styles from blueprint framework (used by polotno)
+// if you bundler doesn't support such import you can use css from CDN (see bellow)
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/popover2/lib/css/blueprint-popover2.css';
+
+const { store } = createDemoApp({
+  container: document.getElementById('root'),
+  key: 'XqIcvDrx6LgQh7yb9-o3', // you can create it here: https://polotno.dev/cabinet/
+  // you can hide back-link on a paid licence
+  // but it will be good if you can keep it for Polotno project support
+  showCredit: true,
+});
+
+
+const page = store.addPage();
+
+page.addElement({
+  x: 50,
+  y: 50,
+  type: 'text',
+  fill: 'black',
+  text: 'hello',
+});
+
+export const App = ({ store }) => {
+  return (
+    <PolotnoContainer style={{ width: '100vw', height: '100vh' }}>
+      <SidePanelWrap>
+        <SidePanel store={store} />
+      </SidePanelWrap>
+      <WorkspaceWrap>
+        <Toolbar store={store} downloadButtonEnabled />
+        <Workspace store={store} />
+        <ZoomButtons store={store} />
+      </WorkspaceWrap>
+    </PolotnoContainer>
+  );
+};
